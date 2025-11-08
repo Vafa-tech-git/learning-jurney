faead61b3c0b1a4c88ab6bd1d3598305aaad2614
Vafa tech <fafaneatavirgilvalentin@gmail.com>
2025-11-08 22:13:07 +0200

    feat: Add solution for learncpp.com Chapter 16.2 Question 4

---
+++ C++/learncpp.com/16.Dynamic arrays std vector/16.2.Introduction to std vector and list constructors/Question_4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+
+std::vector<int> sum(2);
+
+std::cout << "Give 3 integral values separated by a space: ";
+std::cin >> sum[0] >> sum[1] >> sum[2];
+
+std::cout << "The sum is: " << sum[0]+sum[1]+sum[2] << '\n';
+std::cout << "The product is: " << sum[0]*sum[1]*sum[2];
+
+return 0;
+}
